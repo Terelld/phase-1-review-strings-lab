4aa1b84a18dcdf37556a8277ba58e17aaca00ec6
Terell <Terelld@gmail.com>
2022-05-17 06:15:57 -0500

    commit tree e87008ad75fd7b72911317b90a26c112a9c706ef

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Johnny D'
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
